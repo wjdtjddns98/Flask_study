f101afca4f34adebfb2fb0631a5a5087c278df10
wjdtjddns98 <wjdtjddns98@naver.com>
2025-06-04 17:02:32 +0900

    jsonify

--- Part2/jsonify/app.py
+++ Part2/jsonify/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -12,6 +13,27 @@ def show_all_feeds():
 def show_one_feed(feed_id):
     print(feed_id)
     data = {'result': 'success', 'data': {"feed1": "data1"}}
-
-
     return data
+
+
+# POST
+@app.route('/api/v1/feeds', methods=['POST'])
+def create_feed():
+    name = request.form['name']
+    age = request.form['age']
+    print(name, age)
+    return jsonify({'result': 'success'})
+
+datas = [{"items" : [{"name": "item1", "price": 1201233}]}]
+
+@app.route ('/api/v1/feeds/datas', methods=['GET'])
+def get_datas():
+    return {"datas":datas}
+
+@app.route('/api/v1/feeds/datas', methods=['POST'])
+def create_data():
+    request_data = request.get_json()
+
+    new_data = {'items' : request_data.get('items', [])}
+    datas.append(new_data)
+    return new_data, 201
